27571e1b8b1a6fcae4ec744a8d29feaa6e9bd3fb
许大仙 <1900919313@qq.com>
2024-06-29 11:00:17 +0800

    🎉 初始化

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # c-study
 C 学习
+
+> 编辑器使用的是 vs code（Visual Studio Code），而不是 VS（Visual Studio）
---
+++ day01/HelloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int a = 10;
+
+  if (a > 10) {
+    printf("a is greater than 10\n");
+  }
+
+  printf("Hello, World!\n");
+
+  return 0;
+}
