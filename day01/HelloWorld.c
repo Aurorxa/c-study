183d7118e30ab1ca89dce709273d62ff642fde51
许大仙 <1900919313@qq.com>
2024-06-29 14:27:35 +0800

    注释

--- day01/HelloWorld.c
+++ day01/HelloWorld.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include <stdio.h> // 这是编译预处理指令
 
-int main() {
+int main() { // 定义主函数
 
-  printf("Hello, World!\n");
+    printf("你好，世界！！！"); // 输出所指定的一行信息
 
-  return 0;
+    return 0;  // 函数执行完毕时返回函数值0
 }
