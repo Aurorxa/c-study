b24429001781d366242c8f2b7bab956eae52472e
许大仙 <1900919313@qq.com>
2024-07-01 14:04:27 +0800

    变量

--- src/day02/VaiableDemo1.c
+++ src/day02/VaiableDemo1.c
@@ -10,19 +10,5 @@ int main() {
 
     printf("a = %d\n", a);
 
-    // 声明一个整型变量，取名为 b ，并直接赋值
-    int b = 200;
-
-    // 修改变量 b 的值，将变量 a 的值赋值给变量 b
-    b = a;
-
-    printf("b= %d\n", b);
-
-    // 同时声明多个整型的变量并赋值
-    int c1 = 10, c2 = 20, c3 = 30;
-    printf("c1 = %d\n", c1);
-    printf("c2 = %d\n", c2);
-    printf("c3 = %d\n", c3);
-
     return 0;
 }
