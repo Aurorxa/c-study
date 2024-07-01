b645db0c5012a4f1a5a4fe3f06d9913db5823e7b
许大仙 <1900919313@qq.com>
2024-07-01 17:02:51 +0800

    变量

---
+++ src/day02/Enum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+enum Sex {
+    MALE = 1,
+    FEMALE = 2,
+};
+
+int main() {
+    enum Sex sex;
+
+    printf("请输入性别(1 表示男性, 2 表示女性)：");
+    scanf("%d", &sex);
+    printf("您的性别是：%d\n", sex);
+
+    return 0;
+}
