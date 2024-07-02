31c41ac2710b62b3b95f595541c40806ab3fb6c0
许大仙 <1900919313@qq.com>
2024-07-02 14:28:10 +0800

    更改

--- src/day03/Radix.c
+++ src/day03/Radix.c
@@ -3,13 +3,13 @@
 
 int main() {
 
-    int num1 = 0b10;
-    int num2 = 07;
-    int num3 = 1000;
-    int num4 = 0xaf72;
+    int num1 = 0b10100110; // 二进制
+    int num2 = 0717563; // 八进制
+    int num3 = 1000; // 十进制
+    int num4 = 0xaf72; // 十六进制
 
-    printf("num1 = %d\n", num1); // num1 = 2
-    printf("num2 = %d\n", num2); // num2 = 7
+    printf("num1 = %d\n", num1); // num1 = 166
+    printf("num2 = %d\n", num2); // num2 = 237427
     printf("num3 = %d\n", num3); // num3 = 1000
     printf("num4 = %d\n", num4); // num4 = 44914
 
