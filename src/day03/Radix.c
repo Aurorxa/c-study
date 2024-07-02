43eaf0a8047a58ff22ebdf6ace35fe283f8f0982
许大仙 <1900919313@qq.com>
2024-07-02 14:25:47 +0800

    更改

---
+++ src/day03/Radix.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+
+    int num1 = 0b10;
+    int num2 = 07;
+    int num3 = 1000;
+    int num4 = 0xaf72;
+
+    printf("num1 = %d\n", num1); // num1 = 2
+    printf("num2 = %d\n", num2); // num2 = 7
+    printf("num3 = %d\n", num3); // num3 = 1000
+    printf("num4 = %d\n", num4); // num4 = 44914
+
+    return 0;
+}
