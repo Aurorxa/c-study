d3ef5d7edad0b2ec5ffb513725c4f0c39bc347f0
许大仙 <1900919313@qq.com>
2024-07-03 14:42:33 +0800

    整型

--- src/day04/DataTypeDemo1.c
+++ src/day04/DataTypeDemo1.c
@@ -12,19 +12,19 @@ int main() {
     // 整型
     int          b1 = 100;
     signed int   b2 = -100;
-    unsigned int b3 = 200;
+    unsigned int b3 = 200U;
     printf("b1 = %d , b2 = %d , b3 = %d \n", b1, b2, b3);
 
     // 长整型
     long          c1 = 1000L;
     signed long   c2 = -1000L;
-    unsigned long c3 = 1000L;
+    unsigned long c3 = 1000LU;
     printf("b1 = %ld , b2 = %ld , b3 = %ld \n", c1, c2, c3);
 
     // 长长整型
     long long          d1 = 10000LL;
     signed long long   d2 = -10000LL;
-    unsigned long long d3 = 10000LL;
+    unsigned long long d3 = 10000LLU;
     printf("d1 = %lld , d2 = %lld , d3 = %lld \n", d1, d2, d3);
 
     return 0;
