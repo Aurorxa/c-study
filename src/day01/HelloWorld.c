35290d972830a40c170bd437e001aed1e1b2d0d8
许大仙 <1900919313@qq.com>
2024-07-10 15:06:26 +0800

    更改

--- src/day01/HelloWorld.c
+++ src/day01/HelloWorld.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
@@ -6,7 +7,9 @@ int main() {
         printf("%d", i);
     }
 
-    printf("你好，世界！！！");
+    printf("你好，世界！！！\n");
+
+    system("pause");
 
     return 0;
 }
