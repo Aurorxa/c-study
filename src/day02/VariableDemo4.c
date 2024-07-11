fcff51f581cbff4096e3f678b5d7ef598a154be3
许大仙 <1900919313@qq.com>
2024-07-11 15:13:43 +0800

    更改

--- src/day02/VariableDemo4.c
+++ src/day02/VariableDemo4.c
@@ -8,8 +8,8 @@ int main() {
     char result = a + c;
     printf("result = %c\n", result);
 
-    printf("%d\n", &result);
-    printf("%#X\n", &result);
+    printf("%d\n", &a); // %d 以十进制的方式输出地址编号
+    printf("%p\n", &a); // %p 以指针的方式输出地址编号，十六进制
 
     return 0;
 }
