86107fade8ebb11e58ec8bad79f3f73dedc6c314
许大仙 <1900919313@qq.com>
2024-07-15 13:41:53 +0800

    更改

--- src/day02/VariableDemo2.c
+++ src/day02/VariableDemo2.c
@@ -2,5 +2,12 @@
 
 int main() {
 
+    int num = 10;
+
+    printf("变量所占内存空间的大小：%zd字节\n", sizeof(num));
+
+    // 数据类型所占内存空间的大小
+    printf("数据类型所占内存空间的大小：%zd字节\n", sizeof(int));
+
     return 0;
 }
