590e4b6484e2fad7d2a6e749d2b561553c4bbf34
许大仙 <1900919313@qq.com>
2024-07-15 17:01:45 +0800

    更改

--- src/day04/ShortTypeDemo3.c
+++ src/day04/ShortTypeDemo3.c
@@ -4,7 +4,7 @@
 int main() {
 
     printf("short 类型的范围是[%hd,%hd]\n", SHRT_MIN,SHRT_MAX);
-    printf("unsigned short 类型的范围是[%hu,%hu]\n", 0,USHRT_MAX);
+    printf("unsigned short 类型的范围是[0,%hu]\n", USHRT_MAX);
 
     return 0;
 }
