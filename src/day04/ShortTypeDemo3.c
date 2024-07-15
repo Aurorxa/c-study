2328cde2ff3567ba58120805cd3fbeb196865f92
许大仙 <1900919313@qq.com>
2024-07-15 16:59:38 +0800

    更改

--- src/day04/ShortTypeDemo3.c
+++ src/day04/ShortTypeDemo3.c
@@ -1,19 +1,10 @@
+#include <limits.h>
 #include <stdio.h>
 
 int main() {
 
-    // 定义有符号 short 类型
-    signed short s1 = -100;
-
-    printf("%zd\n", sizeof(s1));
-
-    // 定义无符号 short 类型
-    unsigned short s2 = 100;
-    printf("%zd\n", sizeof(s2));
-
-    // 定义 short 类型，默认是有符号
-    short s3 = -200;
-    printf("%zd\n", sizeof(s3));
+    printf("short 类型的范围是[%d,%d]\n", SHRT_MIN,SHRT_MAX);
+    printf("unsigned short 类型的范围是[%d,%d]\n", 0,USHRT_MAX);
 
     return 0;
 }
