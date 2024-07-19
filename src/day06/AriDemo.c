cb3fc16bb23fdf3fbd404aa794a31ea0b70e0a19
许大仙 <1900919313@qq.com>
2024-07-19 08:52:32 +0800

    🎨

--- src/day06/AriDemo.c
+++ src/day06/AriDemo.c
@@ -5,11 +5,11 @@ int main() {
     int a = 5;
     int b = 2;
 
-    printf("%d + %d = %d\n", a, b, a + b);
-    printf("%d - %d = %d\n", a, b, a - b);
-    printf("%d × %d = %d\n", a, b, a * b);
-    printf("%d / %d = %d\n", a, b, a / b);
-    printf("%d %% %d = %d\n", a, b, a % b);
+    printf("%d + %d = %d\n", a, b, a + b); // 5 + 2 = 7
+    printf("%d - %d = %d\n", a, b, a - b); // 5 - 2 = 3
+    printf("%d × %d = %d\n", a, b, a * b); // 5 × 2 = 10
+    printf("%d / %d = %d\n", a, b, a / b); // 5 / 2 = 2
+    printf("%d %% %d = %d\n", a, b, a % b); // 5 % 2 = 1
 
     return 0;
 }
