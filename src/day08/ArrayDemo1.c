a1955daba40e7e975038571c8e4f8a8f7a680d6b
许大仙 <1900919313@qq.com>
2024-07-23 08:41:23 +0800

    数组

--- src/day08/ArrayDemo1.c
+++ src/day08/ArrayDemo1.c
@@ -2,7 +2,12 @@
 
 int main() {
 
-    printf("Hello, World!\n");
+    // 定义数组
+    int arr[3];
+
+    for (int i = 0; i < 3; i++) {
+        printf("第%d个元素的值：%d \n", i, arr[i]);
+    }
 
     return 0;
 }
