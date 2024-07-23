66542e96fb2e5675ab290bb46d337e10f48f961f
许大仙 <1900919313@qq.com>
2024-07-23 08:44:46 +0800

    数组

--- src/day08/ArrayDemo1.c
+++ src/day08/ArrayDemo1.c
@@ -5,8 +5,14 @@ int main() {
     // 定义数组
     int arr[3];
 
+    // 给数组元素赋值
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+
+    // 遍历数组
     for (int i = 0; i < 3; i++) {
-        printf("第%d个元素的值：%d \n", i, arr[i]);
+        printf("第 %d 个元素的值：%d \n", i, arr[i]);
     }
 
     return 0;
