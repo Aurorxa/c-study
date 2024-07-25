da9743bb8b061ab8c50d45b0c11087ba3a2b8fdb
Aurorxa <1900919313@qq.com>
2024-07-25 14:24:29 +0800

    :

--- src/day09/FunDemo1.c
+++ src/day09/FunDemo1.c
@@ -4,10 +4,18 @@ void demo(int num) {
     printf("num = %d\n", num);
 }
 
+void demo2(int num[]) {
+    printf("数组的长度2 = %zu\n", sizeof(num));
+}
+
 int main() {
 
     int num = 10;
     demo(num);
+
+    int num2[] = {1, 2, 3, 4, 5};
+    printf("数组的长度1 = %zu\n", sizeof(num2));
+    demo2(num2);
 
     return 0;
 }
