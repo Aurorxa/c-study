62f46079b132b834d002d01d1760a98ceaacbbc4
Aurorxa <1900919313@qq.com>
2024-07-26 15:29:44 +0800

    :

--- src/day10/Pointer1.c
+++ src/day10/Pointer1.c
@@ -6,10 +6,10 @@ int main() {
     int *ptr;
     // 定义一个整型变量
     int num = 10;
-    printf("num = %d\n", num);
+    printf("num = %d \n", num);
     // 将 num 的地址赋值给 ptr
     ptr = &num;
-    printf("ptr = %p\n", ptr);
+    printf("ptr = %p \n", ptr);
 
     return 0;
 }
