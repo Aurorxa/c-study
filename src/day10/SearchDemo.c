73f444cc4cdbb276117cc6a704b040b37831e838
许大仙 <1900919313@qq.com>
2024-08-08 07:54:17 +0800

    :zap: 修改

---
+++ src/day10/SearchDemo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int search(int arr[], int len, int num) {
+    int min = 0;
+    int max = len - 1;
+    int mid = (min + max) / 2;
+    while (min < max) {
+        if (arr[mid] > num) { // 说明要查找的数据在左半边
+            max = mid - 1;
+        } else if (arr[mid] < num) { // 说明要查找的数据在右半边
+            min = mid + 1;
+        } else { // 说明找到了
+            return mid;
+        }
+        mid = (min + max) / 2;
+    }
+    return -1;
+}
+
+int main() {
+
+    int arr[] = {1, 2, 3, 4, 5, 6};
+
+    int len = sizeof(arr) / sizeof(arr[0]);
+
+    int index = search(arr, len, 5);
+
+    printf("index = %d\n", index);
+
+    return 0;
+}
