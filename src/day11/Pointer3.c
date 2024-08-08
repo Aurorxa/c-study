018e8e81570f2b4b63bbdc5c94452d88e73141cb
许大仙 <1900919313@qq.com>
2024-08-08 11:25:26 +0800

    指针

---
+++ src/day11/Pointer3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void swap(int num1, int num2) {
+    int temp = num1;
+    num1     = num2;
+    num2     = temp;
+}
+
+int main() {
+    // 禁用 stdout 缓冲区
+    setbuf(stdout, NULL);
+
+    /**
+     * 定义两个变量，要求交换两个变量中记录的值
+     * 注意：交换的代码需要写在函数 swap 中
+     */
+    int num1 = 10;
+    int num2 = 20;
+
+    printf("调用前: num1 = %d, num2 = %d\n", num1, num2); // 调用前: num1 = 10, num2 = 20
+    swap(num1, num2);
+
+    printf("调用后: num1 = %d, num2 = %d\n", num1, num2); // 调用后: num1 = 10, num2 = 20
+
+    return 0;
+}
