b799e4216c783ac460f0034cfdb89bde47049dd2
许大仙 <1900919313@qq.com>
2024-08-08 10:59:00 +0800

    Generating commit message...

--- src/day10/StringPointer.c
+++ src/day10/StringPointer.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
-#include <time.h>
 
 int main() {
     // 禁用 stdout 缓冲区
     setbuf(stdout, NULL);
 
-    int a = -10;
+    // 定义变量
+    int num = 10;
 
-    time(NULL);
+    // 访问变量
+    printf("num = %d\n", num);
+
+    // 给变量赋值
+    num = 100;
+
+    // 访问变量
+    printf("num = %d\n", num);
+
+    printf("%zu", sizeof(int *));
 
     return 0;
 }
