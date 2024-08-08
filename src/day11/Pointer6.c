016a564de7ccf92aa6f44094f13dfbd51629b242
许大仙 <1900919313@qq.com>
2024-08-08 13:53:44 +0800

    指针

--- src/day11/Pointer6.c
+++ src/day11/Pointer6.c
@@ -24,6 +24,7 @@ int main() {
     bool ret = getMod(num1, num2, &res);
     if (ret) {
         printf("%d %% %d = %d\n", num1, num2, res);
+        
     } else {
         printf("除数不能为 0\n");
     }
