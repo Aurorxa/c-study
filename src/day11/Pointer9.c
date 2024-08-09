ad1a84a20c0015d94b0e8c7b8715bc7b13ce74fd
许大仙 <1900919313@qq.com>
2024-08-09 08:38:45 +0800

    指针

---
+++ src/day11/Pointer9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // 禁用 stdout 缓冲区
+    setbuf(stdout, NULL);
+
+    // 定义数组
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int len   = sizeof(arr) / sizeof(arr[0]);
+
+    // 获取数组的指针
+    int *p = arr;
+
+    // 遍历数组
+    for (int i = 0; i < len; i++) {
+        printf("%d ", *(p + i));
+    }
+    return 0;
+}
