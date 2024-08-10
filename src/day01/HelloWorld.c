ffffc1b9242da6351808165e8f9e44d78e2456c3
许大仙 <1900919313@qq.com>
2024-08-10 17:47:08 +0800

    指针

--- src/day01/HelloWorld.c
+++ src/day01/HelloWorld.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
 
-    for (int i = 0; i < 10; ++i) {
-        printf("%d", i);
-    }
-
     printf("你好，世界！！！\n");
-
-    system("pause");
 
     return 0;
 }
