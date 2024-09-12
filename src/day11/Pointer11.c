77f3035b233c30c80f2f77f57a4f11f29eb1524a
许大仙 <1900919313@qq.com>
2024-09-12 15:19:25 +0800

    指针

--- src/day11/Pointer11.c
+++ src/day11/Pointer11.c
@@ -4,16 +4,16 @@ int main() {
     // 禁用 stdout 缓冲区
     setbuf(stdout, nullptr);
 
-    // 定义字符串指针数组
-    char *strArr[] = {"Hello", "World"};
+    // 定义一个二维数组
+    int arr[3][4] = {{1, 2, 3, 4},
+                     {5, 6, 7, 8},
+                     {9, 10, 11, 12}};
 
-    // 计算字符穿指针数组的长度
-    int len = sizeof(strArr) / sizeof(strArr[0]);
-
-    // 打印字符串
-    for (int i = 0; i < len; i++) {
-        printf("%s\n", strArr[i]);
-    }
+    printf("arr[0][0] = %d\n", arr[0][0]);   // arr[0][0] = 1
+    printf("&arr[0][0] = %p\n", &arr[0][0]); // &arr[0][0] = 0x7ffe4bd14720，获取 arr[0][0] 元素的地址
+    printf("arr[0] = %p\n", arr[0]);         // arr[0] = 0x7ffe4bd14720，获取 arr[0][0] 的地址
+    printf("arr = %p\n", arr);               // arr = 0x7ffe4bd14720，获取 arr[0] 的地址
+    printf("&arr = %p\n", &arr);             // &arr = 0x7ffe4bd14720，获取二维数组 arr 的地址
 
     return 0;
 }
