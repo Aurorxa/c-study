61708c0b3d0e9fa2e6f5f3a9f5ecd089acd3e020
许大仙 <1900919313@qq.com>
2024-09-19 10:15:01 +0800

    指针

--- src/day11/Pointer10.c
+++ src/day11/Pointer10.c
@@ -2,22 +2,29 @@
 
 int main() {
     // 禁用 stdout 缓冲区
-    setbuf(stdout, NULL);
+    setbuf(stdout, nullptr);
 
-    // 定义一维数组
-    int arr[3] = {1, 2, 3};
+    // 定义二维数组
+    int arr[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
 
-    // 定义指针变量
-    int *p = arr;
+    // 定义二维数组指针
+    int(*p)[4] = arr;
 
-    printf("arr[0] 的地址为 %p\n", arr); // arr[0] 的地址为 0x7ffd580ed19c
-    printf("arr[0] 的地址为 %p\n", p);   // arr[0] 的地址为 0x7ffd580ed19c
+    printf("arr[1] = %p\n", (void *)arr[1]);
+    printf("arr[1] = %p\n", (void *)(p + 1));
+    printf("arr[1] = %p\n", (void *)*(p + 1));
 
-    printf("arr[1] 的地址为 %p\n", arr + 1); // arr[1] 的地址为 0x7ffd580ed1a0
-    printf("arr[1] 的地址为 %p\n", p + 1);   // arr[1] 的地址为 0x7ffd580ed1a0
+    // 计算数组的行和列
+    int rows = sizeof(arr) / sizeof(arr[0]);
+    int cols = sizeof(arr[0]) / sizeof(arr[0][0]);
 
-    printf("arr[2] 的地址为 %p\n", arr + 2); // arr[2] 的地址为 0x7ffd580ed1a4
-    printf("arr[2] 的地址为 %p\n", p + 2);   // arr[2] 的地址为 0x7ffd580ed1a4
+    // 遍历数组元素
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", *(*(p + i) + j));
+        }
+        printf("\n");
+    }
 
     return 0;
 }
