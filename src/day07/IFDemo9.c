1896d2e1ba7e0e46e85f20b0ef79fb94c6b701e2
许大仙 <1900919313@qq.com>
2024-09-21 23:09:24 +0800

    c

--- src/day07/IFDemo9.c
+++ src/day07/IFDemo9.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 // 生成指定范围的随机数的函数
 int randomInRange(int min, int max) {
@@ -10,7 +11,12 @@ int randomInRange(int min, int max) {
 int main() {
 
     // 使用当前时间作为种子
-    srand(time(0));
+    // 获取精确到秒的时间
+    unsigned int seed = (unsigned int) time(NULL);
+    // 使用当前进程 ID 和时间来混合生成种子
+    seed += getpid();
+
+    srand(seed);
 
     // 定义范围
     int min = 1;
