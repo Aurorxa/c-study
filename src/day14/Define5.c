069462e91c5b0b3e87ee8df7c163f4a6c20229a8
许大仙 <1900919313@qq.com>
2024-10-09 17:20:28 +0800

    Here is a possible Git commit message in present tense, in the language 中文, and not longer than 74 characters:  diff 中了 2 行，其中 1 行是删除的，另一行是添加的。

--- src/day14/Define5.c
+++ src/day14/Define5.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
+#if _WIN32 // 如果是windows平台, 就引入 <windows.h>
+#include <windows.h>
+#define SLEEP(t) Sleep(t * 1000)
+#elif __linux__ // 如果是linux平台， 就引入<unistd.h>
+#include <unistd.h>
+#define SLEEP sleep
+#endif
+
 int main() {
 
     // 禁用 stdout 缓冲区
     setbuf(stdout, nullptr);
 
-#if defined(_DEBUG)
-    printf("正在使用 Debug 模式编译程序...\n");
-#elif defined(NDEBUG)
-    printf("正在使用 Release 模式编译程序...\n");
-#elif defined(RELWITHDEBINFO)
-    printf("正在使用 RelWithDebInfo 模式编译程序...\n");
-#elif defined(_MINSIZEREL)
-    printf("正在使用 Minsizerel 模式编译程序...\n");
-#else
-    printf("正在使用未知模式编译程序...\n");
-#endif
+    SLEEP(5);
+    printf("hello, 大家好~");
 
     return 0;
 }
