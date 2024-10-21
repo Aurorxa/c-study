be8595b0cce58104a99ef5b6aa5d4994ef3010cf
许大仙 <1900919313@qq.com>
2024-10-21 16:44:18 +0800

    Here is a possible commit message for the given diff in present tense and 74 characters or less:  Diff: added enum Color with 3 values, switched switch-case formatting.

--- src/day16/demo2.c
+++ src/day16/demo2.c
@@ -1,47 +1,24 @@
-#include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-// 线程执行的函数
-void *thread_function(void *arg) {
-    int thread_num = *(int *)arg; // 获取线程编号
-    printf("线程 %d 正在运行...\n", thread_num);
-
-    // 模拟一些工作
-    for (int i = 0; i < 5; i++) {
-        printf("线程 %d 计数：%d\n", thread_num, i);
-    }
-
-    printf("线程 %d 结束。\n", thread_num);
-    return NULL; // 线程返回值
-}
 
 int main() {
 
     // 禁用 stdout 缓冲区
     setbuf(stdout, nullptr);
 
-    pthread_t thread1, thread2; // 定义两个线程ID
-    int       thread_num1 = 1;
-    int       thread_num2 = 2;
+    int   chinese, math, english;
+    float average;
 
-    // 创建第一个线程
-    if (pthread_create(&thread1, NULL, thread_function, &thread_num1) != 0) {
-        perror("创建线程1失败");
-        return 1;
-    }
+    printf("请输入语文成绩：");
+    scanf("%d", &chinese);
 
-    // 创建第二个线程
-    if (pthread_create(&thread2, NULL, thread_function, &thread_num2) != 0) {
-        perror("创建线程2失败");
-        return 1;
-    }
+    printf("请输入数学成绩：");
+    scanf("%d", &math);
 
-    // 等待两个线程结束
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
+    printf("请输入英语成绩：");
+    scanf("%d", &english);
 
-    printf("主线程结束。\n");
-    
+    average = (chinese + math + english) / 3.0;
+    printf("平均成绩为：%.2f\n", average);
+
     return 0;
 }
