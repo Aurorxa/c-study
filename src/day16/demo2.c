71baba7ce10a6020df58ba81fe62b691ce6509f0
许大仙 <1900919313@qq.com>
2024-10-22 13:56:20 +0800

    Here is a possible commit message for the given diff in present tense and 74 characters or less:  Diff: added enum Color with 3 values, switched switch-case formatting.

--- src/day16/demo2.c
+++ src/day16/demo2.c
@@ -5,20 +5,16 @@ int main() {
     // 禁用 stdout 缓冲区
     setbuf(stdout, nullptr);
 
-    int   chinese, math, english;
-    float average;
+    int  num1, num2;
+    char ch;
+    int  ret = scanf("%d %d %c", &num1, &num2, &ch);
 
-    printf("请输入语文成绩：");
-    scanf("%d", &chinese);
-
-    printf("请输入数学成绩：");
-    scanf("%d", &math);
-
-    printf("请输入英语成绩：");
-    scanf("%d", &english);
-
-    average = (chinese + math + english) / 3.0;
-    printf("平均成绩为：%.2f\n", average);
+    /*
+        若键入的数据是 100 200 A，则正常匹配录入 3 个数据，ret 等于 3
+        若键入的数据是 100 A 200，则正常匹配录入 1 个数据，ret 等于 1
+        若键入的数据是 A 100 200，则正常匹配录入 0 个数据，ret 等于 0
+    */
+    printf("ret = %d\n", ret);
 
     return 0;
 }
