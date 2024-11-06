2103f2e53143d4dca681c318ceee1890f5873c9c
许大仙 <1900919313@qq.com>
2024-11-06 14:01:47 +0800

    Here is a possible commit message for the given diff:  diff 中的更改：重命名 malloc.c 为 malloc1.c，并添加了一个新的数组。

--- src/day17/main.c
+++ src/day17/main.c
@@ -1,10 +1,27 @@
-#include "./include/vector.h"
 #include <stdio.h>
+
+char *getBinary(int num) {
+    static char binaryString[33];
+    int         i, j;
+
+    for (i = sizeof(num) * 8 - 1, j = 0; i >= 0; i--, j++) {
+        const int bit   = (num >> i) & 1;
+        binaryString[j] = bit + '0';
+    }
+
+    binaryString[j] = '\0';
+    return binaryString;
+}
 
 int main() {
 
     // 禁用 stdout 缓冲区
-    setbuf(stdout, nullptr);
+    setbuf(stdout, NULL);
+
+    int num = -10;
+    printf("b=%b\n", num); // b=11111111111111111111111111110110
+    printf("b=%d\n", num); // b=-10
+    printf("b=%u\n", num); // b=4294967286
 
     return 0;
 }
