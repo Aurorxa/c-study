b32debff4ed99678dc0d01d20d6de794f20837d9
许大仙 <1900919313@qq.com>
2024-11-06 14:57:19 +0800

    Here is a possible commit message for the given diff:  diff 中的更改：重命名 malloc.c 为 malloc1.c，并添加了一个新的数组。

--- src/day17/main.c
+++ src/day17/main.c
@@ -3,12 +3,24 @@
 int main() {
 
     // 禁用 stdout 缓冲区
-    setbuf(stdout, NULL);
+    setbuf(stdout, nullptr);
 
-    int num = -10;
-    printf("b=%b\n", num); // b=11111111111111111111111111110110
-    printf("b=%d\n", num); // b=-10
-    printf("b=%u\n", num); // b=4294967286
+    int num = 100;
+
+    // 100 的二进制整数： 1100100
+    printf("%d 的二进制整数： %b\n", num, num);
+    // 100 的十进制整数： 100
+    printf("%d 的十进制整数： %d\n", num, num);
+    // 100 的八进制整数： 144
+    printf("%d 的八进制整数： %o\n", num, num);
+    // 100 的十六进制整数： 64
+    printf("%d 的十六进制整数： %x\n", num, num);
+    // 100 的八进制（前缀）整数： 0144
+    printf("%d 的八进制（前缀）整数： %#o\n", num, num);
+    // 100 的十六进制（前缀）整数： 0x64
+    printf("%d 的十六进制（前缀）整数： %#x\n", num, num);
+    // 100 的十六进制（前缀）整数： 0X64
+    printf("%d 的十六进制（前缀）整数： %#X\n", num, num);
 
     return 0;
 }
