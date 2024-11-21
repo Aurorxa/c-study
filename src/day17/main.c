ab0c4993b2d688751b01b7f98c6c0a775d8d8580
许大仙 <1900919313@qq.com>
2024-11-21 16:42:03 +0800

    feat: 移除汉诺塔函数并添加测试函数

--- src/day17/main.c
+++ src/day17/main.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 
-/**
- * 前提：假设可以移动 n - 1 个盘子，如何移动 n 个盘子？
- * 递归公式：
- *  ① 先将 n - 1 个盘子移动到中间杆，即：A -->B
- *  ② 将最大的盘子移动到目标杆上，即：A --> C
- *  ③ 将中间杆上的盘子移动到目标杆上，即：B --> C
- * 边界条件：当 n = 1 时，只需要移动一次
- * @param n 汉诺塔的层数
- * @return 移动次数
- */
-int hanoi(int n) {
-    if (n == 1) {
-        return 1;
-    }
-    return hanoi(n - 1) * 2 + 1;
+int *test() {
+
+    static int num = 10;
+
+    // 在函数中不要返回指向当前栈帧的指针！！！
+    return &num;
 }
 
 int main() {
@@ -22,15 +13,9 @@ int main() {
     // 禁用 stdout 缓冲区
     setbuf(stdout, nullptr);
 
-    int n;
-    printf("请输入汉诺塔的层数：");
-    scanf("%d", &n);
+    int *p = test();
 
-    // 计算移动次数
-    // S(n) = S(n-1) --> 移动到中间杆
-    //        + 1 --> 最大的移动到目标杆
-    //        + S(n-1) --> 中间杆移动到目标杆
-    printf("移动次数：%d\n", hanoi(n));
+    printf("num = %d\n", *p);
 
     return 0;
 }
