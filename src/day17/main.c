6b0677dcc12b460778330684dd3367e4cd297c41
许大仙 <1900919313@qq.com>
2024-12-09 14:15:08 +0800

    fix

--- src/day17/main.c
+++ src/day17/main.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <time.h>
 
-#define LEN 40
+#define LEN 10
 
 int main() {
 
@@ -12,17 +12,9 @@ int main() {
 
     const Vector *vector = vector_create();
 
-    const clock_t start = clock(); // 开始计时
-
     for (int i = 0; i < LEN; i++) {
         vector_push_back(vector, i + 1);
     }
-
-    const clock_t end = clock(); // 结束计时
-
-    double cpu_time_used = (double)(end - start) / CLOCKS_PER_SEC;
-
-    printf("方法执行时间: %f 秒\n", cpu_time_used);
 
     // 遍历动态数组
     for (int i = 0; i < vector->size; i++) {
