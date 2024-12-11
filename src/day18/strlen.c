458090d3fa8323f53674d54d438d490937d351a9
许大仙 <1900919313@qq.com>
2024-12-11 16:48:40 +0800

    feat(day18): 添加官方实现的字符串长度获取函数
    - 新增 strlen2 函数，使用官方实现方式
    - 更新 main 函数，使用 strlen2 替代原有的 strlen 函数
    - 添加函数注释，说明 strlen2 的实现原理

--- src/day18/strlen.c
+++ src/day18/strlen.c
@@ -14,16 +14,31 @@ size_t strlen(const char *str) {
     return len;
 }
 
+/**
+ * 获取字符串长度（官方实现）
+ * @param str
+ * @return
+ */
+size_t strlen2(const char *str) {
+    const char *p = str;
+    // 循环结束条件是遇到字符串结束符
+    while (*str != '\0') {
+        str++;
+    }
+    // 返回字符串长度
+    return str - p;
+}
+
 int main() {
 
     // 禁用 stdout 缓冲区
     setbuf(stdout, NULL);
 
     char str[] = "Hello World";
-    printf("strlen(\"%s\") = %zu\n", str, strlen(str));
+    printf("strlen(\"%s\") = %zu\n", str, strlen2(str));
 
     char str2[] = "HelloWorld";
-    printf("strlen(\"%s\") = %zu\n", str2, strlen(str2));
+    printf("strlen(\"%s\") = %zu\n", str2, strlen2(str2));
 
     return 0;
 }
