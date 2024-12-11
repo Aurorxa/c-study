ab80405ee442864309106060cf6e040af3593076
许大仙 <1900919313@qq.com>
2024-12-11 08:46:53 +0800

    refactor(day18): 重构 main.c 文件
    - 移除了反转数组和打印数组的函数- 添加了动态内存分配和释放的示例代码
    - 增加了空指针检查的示例

--- src/day18/main.c
+++ src/day18/main.c
@@ -1,53 +1,24 @@
 #include <stdio.h>
-#define ARR_SIZE(a) (sizeof(a) / sizeof(a[0]))
-
-/**
- * 反转数组
- * @param arr 数组
- * @param len 长度
- */
-void reverse(int arr[], const int len) {
-    for (int start = 0, end = len - 1; start <= end; start++, end--) {
-        const int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-    }
-}
-
-/**
- * 打印整数数组
- *
- * @param arr 需要打印的整数数组
- * @param len 数组的长度
- *
- * 此函数通过遍历数组并以逗号分隔的方式打印数组中的元素
- * 数组用方括号包围，最后一个元素后面没有逗号
- */
-void printArray(int arr[], const int len) {
-    printf("[");
-    for (int i = 0; i < len; i++) {
-        // 判断是否为最后一个元素
-        if (i == len - 1) {
-            // 是最后一个元素，打印元素并关闭数组括号
-            printf("%d]\n", arr[i]);
-            return;
-        }
-        // 不是最后一个元素，打印元素并添加逗号分隔
-        printf("%d, ", arr[i]);
-    }
-}
+#include <stdlib.h>
 
 int main() {
 
     // 禁用 stdout 缓冲区
     setbuf(stdout, NULL);
 
-    // 定义普通数组
-    int arr[] = {1, 2, 3, 4, 5, 6};
-
-    reverse(arr, ARR_SIZE(arr));
-
-    printArray(arr, ARR_SIZE(arr));
+    // 动态分配内存
+    int *ptr = (int *)malloc(sizeof(int));
+    // 释放内存
+    free(ptr);
+    // 防止悬空指针
+    ptr = NULL;
+    // ptr 是空指针
+    if (ptr != NULL) {
+        *ptr = 20;
+        printf("ptr = %d\n", *ptr);
+    } else {
+        printf("ptr is NULL\n");
+    }
 
     return 0;
 }
