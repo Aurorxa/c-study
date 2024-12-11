dd5ae52400e9135df27b52535fa883b73b3c0007
许大仙 <1900919313@qq.com>
2024-12-11 17:02:02 +0800

    feat(day18): 实现 strcpy 函数
    - 新增 strcpy函数，用于将一个字符串拷贝到另一个字符串中
    - 在 main 函数中测试 strcpy 函数，验证其正确性
    - 添加必要的头文件和禁用 stdout 缓冲区

---
+++ src/day18/strcpy.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * 将一个字符串拷贝到另一个字符串中
+ * @param dest
+ * @param src
+ * @return
+ */
+char *strcpy(char *dest, const char *src) {
+
+    while (*src != '\0') {
+        *dest++ = *src;
+        *src++;
+    }
+    *dest = '\0';
+    return dest;
+}
+
+int main() {
+
+    // 禁用 stdout 缓冲区
+    setbuf(stdout, NULL);
+
+    char src[] = "Hello World";
+    char dest[20];
+
+    strcpy(dest, src);
+
+    printf("%s\n", dest);
+
+    return 0;
+}
