c2d64db82aa63b0151a1afee6eaaa452c1997424
许大仙 <1900919313@qq.com>
2024-12-19 16:05:26 +0800

    feat(linkedlist): 实现单链表的头插法并添加示例
    - 新增 insert_head 函数，用于在链表头部插入新节点
    - 在 main函数中使用头插法创建一个链表并打印第一个节点的值
    - 优化了代码注释，使其更加清晰易懂

--- src/day19/linkedlist.c
+++ src/day19/linkedlist.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node {
     int data;
     struct node *next;
 } Node;
 
+// 头插法
+Node *insert_head(Node *list, int data) {
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    if (new_node == NULL) {
+        printf("malloc failed\n");
+        exit(1);
+    }
+    new_node->data = data;
+    new_node->next = list;
+    return new_node;
+}
+
 int main() {
 
     // 禁用 stdout 缓冲区
     setbuf(stdout, nullptr);
 
-    // list 是一个单链表，等于 NULL 说明是空链表
+    // list 是一个单链表，等于 NULL 就是空链表
     Node *list = NULL;
+
+    list = insert_head(list, 5);
+    list = insert_head(list, 4);
+    list = insert_head(list, 3);
+    list = insert_head(list, 2);
+    list = insert_head(list, 1);
+
+    printf("list: %d\n", list->data);
 
     return 0;
 }
