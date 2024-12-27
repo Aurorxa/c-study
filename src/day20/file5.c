7ffef48c2456eada3456ff98c9080c3aca1a8ce8
许大仙 <1900919313@qq.com>
2024-12-27 14:45:13 +0800

    feat(day20): 删除 file1.txt 并添加 file5.c- 删除了 src/day20/resources/file1.txt 文件，内容为 "abc" - 在 src/day20 目录下新增 file5.c 文件 - 新增的 file5.c 文件实现了一个简单的文件复制功能：   - 打开源文件和目标文件   - 读取源文件内容并写入目标文件   - 关闭文件流   - 打印成功复制消息

---
+++ src/day20/file5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    // 禁用 stdout 缓冲区
+    setbuf(stdout, nullptr);
+
+    // ① 打开文件流
+    FILE *src_file = fopen("/root/西游记.txt", "r");
+    FILE *dest_file = fopen("/root/西游记2.txt", "w");
+
+    if (src_file == NULL) {
+        fprintf(stderr, "Error: Failed to open file %s.\n", "西游记.txt");
+        exit(EXIT_FAILURE);
+    }
+
+    if (dest_file == NULL) {
+        fprintf(stderr, "Error: Failed to open file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // ② 读写文件（统计、转换、加密、解密...）
+    char buffer[4096];
+    size_t len;
+    while ((len = fread(buffer, sizeof(char), sizeof(buffer), src_file)) > 0) {
+        fwrite(buffer, sizeof(char), len, dest_file);
+    }
+
+    // ③ 关闭文件流
+    fclose(src_file);
+    fclose(dest_file);
+
+    printf("文件复制成功！\n");
+
+    return 0;
+}
