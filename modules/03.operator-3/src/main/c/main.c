6add709e70e97b3cf7af05273517768fa99b9843
weiwei.xu <1900919313@qq.com>
2026-08-17 14:53:38 +0800

    feat(operator-3): 添加单目操作符 ++ 和 -- 示例

--- modules/03.operator-3/src/main/c/main.c
+++ modules/03.operator-3/src/main/c/main.c
@@ -3,5 +3,40 @@
 #include "c_study.h"
 
 int main() {
+    /*
+     * 单目操作符：++、--
+     *
+     * 前置++：先自增，再使用
+     * 后置++：先使用，再自增
+     * 前置--：先自减，再使用
+     * 后置--：先使用，再自减
+     */
+
+    int a = 10;
+    int b = ++a;
+    printf("a = %d\n", a); // a = 11
+    printf("b = %d\n", b); // b = 11
+
+    printf_separator();
+
+    a = 10;
+    b = a++;
+    printf("a = %d\n", a); // a = 11
+    printf("b = %d\n", b); // b = 10
+
+    printf_separator();
+
+    a = 10;
+    b = --a;
+    printf("a = %d\n", a); // a = 9
+    printf("b = %d\n", b); // b = 9
+
+    printf_separator();
+
+    a = 10;
+    b = a--;
+    printf("a = %d\n", a); // a = 9
+    printf("b = %d\n", b); // b = 10
+
     return 0;
 }
