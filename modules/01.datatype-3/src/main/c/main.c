51eccf33dac62a923ab299a3ab1dd0fbc85d011f
weiwei.xu <1900919313@qq.com>
2026-08-17 12:12:00 +0800

    feat(01.datatype): 添加 signed/unsigned 示例并更新 sizeof 说明

---
+++ modules/01.datatype-3/src/main/c/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#include "c_study.h"
+
+/*
+ * C 语言使用 signed 和 unsigned 关键字来修饰 字符型 和 整型 ，即：声明有符号类型（默认）和无符号类型。
+ * 对于 signed 关键字而言，表示一个类型带有正负号，可以表示正负整数和 0 ，如：int 类型。
+ * 对于 unsigned 关键字而言，表示一个类型不带有正负号，只能表示正整数和 0 ，如：unsigned int 类型。
+ *
+ * 对于 int 类型，默认是带有正负号的，即：int =  signed int 。
+ *
+ *
+ *
+ */
+int main() {
+    /*
+     * 在生活中，有些数据是没有正负号的，如：年龄、体重、身高等，这种数据就适合使用 unsigned 关键字来修饰。
+     */
+    unsigned int age = 18;
+    unsigned int weight = 50;
+    unsigned int height = 170;
+    printf("年龄：%u\n", age);
+    printf("体重：%u\n", weight);
+    printf("身高：%u\n", height);
+
+    printf_separator();
+
+    /*
+     * 在生活中，有些数据是有正负号的，如：温度、距离、速度等，这种数据就适合使用 signed 关键字来修饰。
+     */
+    signed int temperature = 26;
+    signed int distance = 100;
+    signed int speed = 100;
+    printf("温度：%d\n", temperature);
+    printf("距离：%d\n", distance);
+    printf("速度：%d\n", speed);
+    return 0;
+}
