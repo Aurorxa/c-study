a472f127b472b301345ea95753c3994e8286182e
weiwei.xu <1900919313@qq.com>
2026-08-21 09:30:40 +0800

    test(debug): 更新阶乘模块输出用例

--- modules/09.debug/01.factorial/src/main/c/main.c
+++ modules/09.debug/01.factorial/src/main/c/main.c
@@ -28,6 +28,15 @@ static long long sum(int n) {
 }
 
 int main() {
-    printf("%lld\n", sum(10));
+    printf("%lld\n", sum(1));  // 1
+    printf("%lld\n", sum(2));  // 3
+    printf("%lld\n", sum(3));  // 9
+    printf("%lld\n", sum(4));  // 33
+    printf("%lld\n", sum(5));  // 153
+    printf("%lld\n", sum(6));  // 873
+    printf("%lld\n", sum(7));  // 5913
+    printf("%lld\n", sum(8));  // 46233
+    printf("%lld\n", sum(9));  // 409113
+    printf("%lld\n", sum(10)); // 4037913
     return 0;
 }
