5eed2951464aea197f08b78e004238da1247f2ef
weiwei.xu <1900919313@qq.com>
2026-08-25 10:14:48 +0800

    feat(recursion): 添加递归阶乘示例

--- modules/10.recursion/02.recursion/src/main/c/main.c
+++ modules/10.recursion/02.recursion/src/main/c/main.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
 
-/**
- * 阶乘
- *  n! = n * (n-1) * (n-2) * ... * 1
- * @param n >=1
- * @return
- */
-static long long factorial(const int n) {
-    if (n == 0 || n == 1) {
-        return 1;
-    }
-    return n * factorial(n - 1);
+static void test(int arr[]) {
+    printf("%d\n", arr[0]);
+    printf("%d\n", arr[1]);
+    printf("%d\n", arr[2]);
+    printf("%d\n", arr[3]);
+    printf("%d\n", arr[4]);
+    printf("%d\n", arr[5]);
+    printf("%d\n", arr[6]);
+    printf("%d\n", arr[7]);
+    printf("%d\n", arr[8]);
+    printf("%d\n", arr[9]);
 }
 
 int main() {
-    printf("%lld\n", factorial(0));  // 1
-    printf("%lld\n", factorial(1));  // 1
-    printf("%lld\n", factorial(2));  // 2
-    printf("%lld\n", factorial(3));  // 6
-    printf("%lld\n", factorial(4));  // 24
-    printf("%lld\n", factorial(5));  // 120
-    printf("%lld\n", factorial(6));  // 720
-    printf("%lld\n", factorial(7));  // 5040
-    printf("%lld\n", factorial(8));  // 40320
-    printf("%lld\n", factorial(9));  // 362880
-    printf("%lld\n", factorial(10)); // 3628800
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    test(arr);
     return 0;
 }
