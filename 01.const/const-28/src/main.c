a18e664ee46e2bf9d0921ed509729ed6e4241268
weiwei.xu <1900919313@qq.com>
2026-09-08 12:27:05 +0800

    refactor(const-28): 重构加法示例以使用变量传参

--- 01.const/const-28/src/main.c
+++ 01.const/const-28/src/main.c
@@ -6,7 +6,13 @@ static int add(int a, int b) {
 
 int main() {
 
-    printf("%d\n", add(1, 2));
+    int a = 10;
+    int b = 20;
+    int c = 0;
+
+    c = add(a, b);
+
+    printf("c = %d\n", c);
 
     return 0;
 }
